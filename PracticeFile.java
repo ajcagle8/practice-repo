e8ca5519341d9a96dfe2b50633f4b63f25a2bbbe
ajcagle8 <ajcagle@ucsd.edu>
2022-05-01 22:57:28 -0700

    Added a test file

--- PracticeFile.java
+++ PracticeFile.java
@@ -1,3 +1,21 @@
-public class SkillDemo {
+public class PracticeFile {
 
+    public static int factorial(int n) {
+        if (n <= 0) {
+            System.out.println("Invalid input!");
+            return -1;
+        }
+        
+        int factorial = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            factorial = i * factorial;
+        }
+
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
 }
